2da357afe5c282da39bf4fb05dc254f7d10c68f3
paszo <paszo2008@gmail.com>
2020-07-11 19:59:19 +0200

    Added views index and 404 generated by ejs

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+const openweatherkey = process.env.OPENWEATHERKEY;
+
+console.log(openweatherkey);
+
+app.use(express.static(path.resolve(__dirname, "views")));
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.render("index");
+});
+
+app.use((req, res) => {
+    res.status(404).render("404");
 })
+
+
 
 const port = process.env.PORT || 3000;
 
